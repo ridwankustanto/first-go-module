3f53dbba4751a7e66373e4d00b035236d7fe3757
Ridwan Kustanto <ridwankustanto@gmail.com>
2020-06-08 10:27:52 +0700

    RK: add new function major update

---
+++ v2/hello.go
@@ -0,0 +1,17 @@
+package hello
+
+import (
+	quoteV3 "rsc.io/quote/v3"
+)
+
+func Hello() string {
+	return quoteV3.HelloV3()
+}
+
+func Proverb() string {
+	return quoteV3.Concurrency()
+}
+
+func Glass() string {
+	return quoteV3.GlassV3()
+}
